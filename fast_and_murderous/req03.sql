3d3ec7ec9dd2b2bc42c6c7e1639ce4b380a006d5
Elena Villepreux <elena.villepreux@epita.fr>
2023-12-04 17:46:55 +0100

    third

--- fast_and_murderous/req03.sql
+++ fast_and_murderous/req03.sql
@@ -1,4 +1,4 @@
-SELECT person_id, case_id, content, description
+SELECT person_id, justice.cases.id, content, description
 FROM justice.testimonies, justice.cases
 WHERE case_id = justice.cases.id and content LIKE '%Nexus N3%'
 and (description SIMILAR TO '%speeding%'
