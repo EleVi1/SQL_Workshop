cb85dc784e9736bb590bf866982a0acdfa016240
Elena Villepreux <elena.villepreux@epita.fr>
2023-12-05 17:19:35 +0100

    sec

--- daily_gazette/req01.sql
+++ daily_gazette/req01.sql
@@ -3,14 +3,14 @@ CREATE TABLE if not exists rr_times.locales
 (
     language_code char(2) NOT NULL,
     country_code char(2) NOT NULL,
-    name varchar(50) UNIQUE,
+    name varchar(50) UNIQUE NOT NULL,
 
     UNIQUE (language_code, country_code)
 );
 
 CREATE TABLE if not exists rr_times.translated_articles
 (
-    id serial UNIQUE,
-    title text,
-    body text
+    id serial PRIMARY KEY,
+    title text NOT NULL,
+    body text NOT NULL
 );
