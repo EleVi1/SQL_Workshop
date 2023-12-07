7060f0fb8beb5d4586f2bf361e7d3079cf9b9a41
Elena Villepreux <elena.villepreux@epita.fr>
2023-12-07 12:33:26 +0100

    second

--- vault_cataloging/req03.sql
+++ vault_cataloging/req03.sql
@@ -1,13 +1,7 @@
 SELECT filename,
-    CASE WHEN decrypted = True
-        THEN LEAST(rsa_time, hyper_pulse_time, quantum_x_time, aes_time, 
-        d_crypt_time)
-        ELSE NULL
-    END as best_time,
-CASE WHEN decrypted = True
-        THEN GREATEST(rsa_time, hyper_pulse_time, quantum_x_time, aes_time, 
-        d_crypt_time)
-        ELSE NULL
-    END as worst_time
+LEAST(rsa_time, hyper_pulse_time, quantum_x_time, aes_time, d_crypt_time)
+as best_time,
+GREATEST(rsa_time, hyper_pulse_time, quantum_x_time, aes_time, d_crypt_time)
+as worst_time
 FROM dtf.madelines_files_results
 ORDER BY filename;
